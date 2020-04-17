acf7e6201f1ece6a6ead2d3d7482a3b3771de192
Lara Evdokimova <larisa_evdokimova@epam.com>
2020-04-17 21:59:52 +0300

    NEW SKILL FOR MENTION ALL

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const VkBot = require('node-vk-bot-api')
-const {TOKEN, GROUP_ID} = require ('./secret');
-const {botName} = require('./config');
+const { TOKEN, GROUP_ID } = require ('./secret');
+const { botName, IS_DEBUG } = require('./config');
 
 const skillList = require('./skills/skillList');
 
@@ -21,6 +21,7 @@ bot.on((ctx) => {
             const regCallName = new RegExp(callName, 'i');
             if (regCallName.test(message.test)){
                 console.log('match: ${callName}');
+                IS_DEBUG && console.log('match: ${callName}');
                 skillList[callName](message, bot, GROUP_ID);
                 return;
             }
--- skills/mentionAll/index.js
+++ skills/mentionAll/index.js
@@ -1,15 +1,21 @@
 const mentionAll = {
-callName: '@all',
+    callName: '[@all |позови всех|]',
 action: (message, bot, group_id) => {
     bot.execute('messages.getConversationMembers',
     { peer_id: message.peer_id,
         group_id,
     }
     ).then( res => {
-        console.log(res);
-    })
-
-console.log('вызвали');
+        const membersIds = res.profiles.map(profile => profile.id);
+        constmembersIdsWithAt = membersIds.map(id => '@id${id}');
+        bot.sendMessage(message.peer_id, 'Именем админа, призываю всех: ${membersIdsWithAt.toString()}').catch()
+        err => console.log(err)
+        //res => console.log(res)
+        //console.log(membersIds);
+    )
+    }).catch(
+        err => console.log(err)    
+        )
   }
 }
 
